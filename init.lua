1e5ab982b90bb13c082d0680b713e4eb0e574acb
Jorge Fernandez <jorge.fernandez@uala.com.ar>
2024-10-06 20:18:08 -0300

    refactor(kick): gitsings

--- init.lua
+++ init.lua
@@ -6,20 +6,6 @@ require('lazy').setup({
   'tpope/vim-sleuth',
 
   {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-  {
     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter',
