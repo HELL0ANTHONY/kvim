8bc661042f733873b5b85ef33a819fc3960e3503
Jorge Fernandez <jorge.fernandez@uala.com.ar>
2024-10-14 21:02:36 -0300

    refactor(kick): optimization of cmp

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -4,53 +4,49 @@ return {
   dependencies = {
     'VonHeikemen/lsp-zero.nvim',
     'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'saadparwaiz1/cmp_luasnip',
     {
       'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
+      lazy = true,
+      build = (not vim.fn.has 'win32' == 1 and vim.fn.executable 'make' == 1) and 'make install_jsregexp' or nil,
       dependencies = {
         {
           'rafamadriz/friendly-snippets',
+          lazy = true,
+          event = 'InsertCharPre',
           config = function()
             require('luasnip.loaders.from_vscode').lazy_load()
           end,
         },
       },
     },
+    {
+      'hrsh7th/cmp-cmdline',
+      event = { 'CmdlineEnter' },
+      config = function()
+        local cmp = require 'cmp'
+        cmp.setup.cmdline(':', {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = 'path' },
+          }, {
+            { name = 'cmdline', option = { ignore_cmds = { 'Man', '!' } } },
+          }),
+        })
+        cmp.setup.cmdline('/', {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = { { name = 'buffer' } },
+        })
+      end,
+    },
   },
 
   config = function()
     local cmp = require 'cmp'
-    local cmp_action = require('lsp-zero').cmp_action()
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
     local luasnip = require 'luasnip'
     luasnip.config.setup {}
-
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' },
-      },
-    })
-
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({ { name = 'path' } }, {
-        {
-          name = 'cmdline',
-          option = { ignore_cmds = { 'Man', '!' } },
-        },
-      }),
-    })
 
     cmp.setup {
       snippet = {
@@ -58,41 +54,31 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
-      completion = { completeopt = 'menu,menuone,noinsert' },
-
+      completion = { completeopt = 'menu,menuone,noinsert', debounce = 50, max_item_count = 10 },
       mapping = cmp.mapping.preset.insert {
-        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+        ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm { select = true },
-
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-        -- TODO: Asignar nuevos keys
-        -- ['<C-h>'] = cmp_action.luasnip_jump_forward(),
-        -- ['<C-l>'] = cmp_action.luasnip_jump_backward(),
-
-        ['<Tab>'] = cmp_action.luasnip_supertab(),
-        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
+          else
+            fallback()
           end
         end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
             luasnip.jump(-1)
+          else
+            fallback()
           end
         end, { 'i', 's' }),
       },
-      sources = {
-        { name = 'lazydev', group_index = 0 },
+      sources = cmp.config.sources {
         { name = 'nvim_lsp' },
-        { name = 'luasnip', keyword_length = 2 },
-        { name = 'buffer', keyword_length = 3 },
+        { name = 'luasnip', keyword_length = 3 },
+        { name = 'buffer', keyword_length = 5, max_item_count = 10 },
         { name = 'path' },
       },
     }
