11a5fc827673e3a22779b6ee60d169b65e6bf007
Jorge Fernandez <jorge.fernandez@uala.com.ar>
2025-02-11 10:22:02 -0300

    feat(kick): new hop shortcuts

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -24,6 +24,30 @@ return {
         end,
         desc = '[w]orkspace: jump to [W]ord (multi-window)',
       },
+      {
+        '<leader>wF',
+        function()
+          local directions = require('hop.hint').HintDirection
+
+          require('hop').hint_char1 {
+            direction = directions.BEFORE_CURSOR,
+            current_line_only = true,
+          }
+        end,
+        desc = '[w]orkspace: jump to [F] character before cursor (current line)',
+      },
+      {
+        '<leader>wf',
+        function()
+          local directions = require('hop.hint').HintDirection
+
+          require('hop').hint_char1 {
+            direction = directions.AFTER_CURSOR,
+            current_line_only = true,
+          }
+        end,
+        desc = '[w]orkspace: jump to [f] character after cursor (current line)',
+      },
     }
   end,
   opts = {
