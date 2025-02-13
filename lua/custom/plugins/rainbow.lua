778375885e8b81ce4274742201371e9fe01148c0
HELL0ANTHONY <george.anthony100@hotmail.com>
2025-02-12 23:14:51 -0300

    rainbow - avoid bug

--- lua/custom/plugins/rainbow.lua
+++ lua/custom/plugins/rainbow.lua
@@ -1,15 +1,18 @@
 -- https://gitlab.com/HiPhish/rainbow-delimiters.nvim
 -- https://gitlab.com/HiPhish/rainbow-delimiters.nvim/-/blob/master/doc/rainbow-delimiters.txt
 -- https://gitlab.com/HiPhish/rainbow-delimiters.nvim
+
 local plugin = {
   'HiPhish/rainbow-delimiters.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
 }
 
 function plugin.config()
-  local rainbow_delimiters = require 'rainbow-delimiters'
+  local ok, rainbow_delimiters = pcall(require, 'rainbow-delimiters')
+  if not ok then
+    return
+  end
 
-  -- Definir colores de manera programática
   local colors = {
     Blue = 'Blue',
     Cyan = 'Cyan',
@@ -24,7 +27,8 @@ function plugin.config()
     vim.cmd(('highlight link RainbowDelimiter%s %s'):format(name, link))
   end
 
-  -- Configuración de estrategias y queries
+  local rainbow_parens = 'rainbow-parens'
+
   vim.g.rainbow_delimiters = {
     strategy = {
       [''] = rainbow_delimiters.strategy['global'],
@@ -33,14 +37,14 @@ function plugin.config()
     query = {
       [''] = 'rainbow-delimiters',
       lua = 'rainbow-blocks',
-      typescript = 'rainbow-parens',
-      javascript = 'rainbow-parens',
-      typescriptreact = 'rainbow-parens',
-      javascriptreact = 'rainbow-parens',
-      tsx = 'rainbow-parens',
-      jsx = 'rainbow-parens',
-      html = 'rainbow-parens',
-      go = 'rainbow-parens',
+      go = rainbow_parens,
+      html = rainbow_parens,
+      typescript = rainbow_parens,
+      javascript = rainbow_parens,
+      typescriptreact = rainbow_parens,
+      javascriptreact = rainbow_parens,
+      tsx = rainbow_parens,
+      jsx = rainbow_parens,
     },
     highlight = vim.tbl_keys(colors),
   }
